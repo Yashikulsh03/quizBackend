fc53929456846d2c7f78adeba505348ae46aa7da
Yashikulsh03 <yashikulsh03@gmail.com>
2024-06-04 17:10:25 +0530

    commit

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
-require("dotenv").config();
-const express=require("express");
-const mongoose=require("mongoose");
-const cors=require("cors");
-const checkError = require("./middleware/error");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const checkError = require('./middleware/error');
+const bodyParser=require("body-parser");
 
-
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use((error, req, res, next) => {
     console.log(error);
-    res.status(500).json({ errorMessage: "Something went wrong!" });
+    res.status(500).json({ errorMessage: 'Something went wrong!' });
 });
 
-app.use("/", require("./routes/user"));
-app.use("/api/quiz",require("./routes/quiz"));
+app.use('/', require('./routes/user'));
+app.use('/api/quiz', require('./routes/quiz'));
 
 app.use(checkError);
 
-
-
 mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
-        console.log("Db connected!");
+        console.log('DB connected!');
     })
     .catch((error) => {
-        console.log("Db failed to connect", error);
+        console.log('DB failed to connect', error);
     });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Backend server listening at port: ${PORT}`);
 });
-
-
